41ce5e49f3bc0844bba9ec61cca95873e7ad68f6
Jason Clarke <jclarke@student.neumont.edu>
2020-04-24 23:53:20 -0600

    ai ground work

---
+++ Assets/Scripts/Common/Damagable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour {
+
+[SerializeField] float m_health = 100;
+[SerializeField] Damage m_damage = null;
+[SerializeField] [Range(-1,1)]float m_damageReduction = 0;
+
+private float maxHealth;
+
+public float MaxHealth { get => maxHealth; set => maxHealth = value; }
+public float health { get => m_health; set => m_health = value; }
+public bool destroyed { get; set; } = false;
+public float DamageReduction { get => m_damageReduction; set => m_damageReduction = value; }
+private void Start() { MaxHealth = health; }
+
+public void ApplyDamage(float damageAmount) {
+	health = health - (damageAmount - (damageAmount*DamageReduction));
+	if (!destroyed && health <= 0) {
+	Destroy(gameObject);
+	destroyed = true;
+
+	if (m_damage != null) {
+	Damage damage = Instantiate(m_damage, transform.position, Quaternion.identity);
+	damage.Spawn(transform.position, Vector3.zero, Vector3.up);
+	}
+}
+	
+}
+
+}
---
+++ Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+[SerializeField] float Damage = 0.0f;
+[SerializeField] float Range = 0.0f;
+[SerializeField] float Rate = 0.0f;
+[SerializeField] Damagable Fortitude = null;
+[SerializeField] GameObject Ammo = null;
+[SerializeField] List<GameObject> Barrels;
+
+}
