d83e9c93aac9d3b55dfbdf543fecde5091203368
Jason Clarke <jclarke@student.neumont.edu>
2020-04-30 11:23:02 -0600

    monster killer

---
+++ Assets/MonsterGoal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MonsterGoal : MonoBehaviour {
+
+[SerializeField] float range = 1.0f;
+[SerializeField] string monster = "Monster";
+void Update() {
+    GameObject[] gameObjects = AIUtilities.GetGameObjects(gameObject, monster, range);
+    if (gameObjects != null) { 
+    gameObjects.ToList().ForEach(go => Destroy(go));;
+    }
+}
+
+private void OnDrawGizmosSelected() {
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(transform.position, range);
+}
+
+}
