edc6344a8f763661e55088f13413b1c7b9fff08b
Jason Clarke <jclarke@student.neumont.edu>
2020-05-06 18:55:41 -0600

    currency & map update

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
 
 [SerializeField] NavMeshSurface mesh = null;
+[SerializeField] int currency = 100;
+[SerializeField] Text currencyDisplay = null;
 
 public static bool Rebuild = false;
-public static int Curency { get; set; } = 0;
+public int Currency { get => currency; set => currency = value; }
 
 void Update() {
+    currencyDisplay.text = $"Currency: {currency}";
     if (Input.GetKeyDown(KeyCode.G)) { UpdateNavMesh(); }
     if (Rebuild) UpdateNavMesh(); Rebuild = false;
 }
