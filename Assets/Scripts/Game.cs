853ef7a91f68c026535a2523b6a5176df2b8c50d
Jason Clarke <jclarke@student.neumont.edu>
2020-05-07 14:31:17 -0600

    pause menu

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,17 +8,29 @@ public class Game : MonoBehaviour {
 
 [SerializeField] NavMeshSurface mesh = null;
 [SerializeField] int currency = 100;
+[SerializeField] int coreHealth = 100;
 [SerializeField] Text currencyDisplay = null;
 [SerializeField] GameObject panel = null;
+[SerializeField] GameObject pauseMenu = null;
 
+private bool paused = false;
 public static bool Rebuild = false;
 public int Currency { get => currency; set => currency = value; }
 
 void Update() {
     currencyDisplay.text = $"Currency: {currency}";
-    if (Input.GetKeyDown(KeyCode.G)) { UpdateNavMesh(); }
-    if (Rebuild) UpdateNavMesh(); Rebuild = false;
-    if (Input.GetKeyDown(KeyCode.M)) panel.SetActive((panel.activeSelf) ? false : true);
+    if (Input.GetKeyDown(KeyCode.Escape)) { PausePlayGame(); }
+    if (!paused && Input.GetKeyDown(KeyCode.G)) { UpdateNavMesh(); }
+    if (!paused && Rebuild) UpdateNavMesh(); Rebuild = false;
+    if (!paused && Input.GetKeyDown(KeyCode.M)) panel.SetActive((panel.activeSelf) ? false : true);
+}
+
+public void PausePlayGame() {
+    paused = (paused) ? false : true;
+    if (paused) Cursor.lockState = CursorLockMode.None;
+    else Cursor.lockState = CursorLockMode.Locked;
+    Time.timeScale = (paused) ? 0: 1;
+    pauseMenu.SetActive(paused);
 }
 
 public void UpdateNavMesh() { 
