2cfed9f3daefd3ec04d77aea6012f893f3afc065
YooperScum <47546105+YooperScum@users.noreply.github.com>
2020-05-11 14:27:27 -0600

    Splash Damage Tower and Timed Traps
    Splash Damage Projectile Implemented
    Timer Trap script implemented
    Splash Tower TD prefab added
    Timer Trap prefab added

---
+++ Assets/Scripts/Defences/Towers/SplashProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashProjectile : Projectile
+{
+    public float damage = 1.0f;
+    public float radius = 10f;
+
+    Vector3 direction;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Monster")
+        {
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), gameObject.GetComponent<Collider>(), true);
+        }
+        else
+        { 
+            Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, radius);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.gameObject.CompareTag("Monster"))
+                {
+                    collider.GetComponent<Damagable>().ApplyDamage(damage);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Defences/Traps/TD/TimerTrap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrap : TrapTD
+{
+    public float timer = 5f;
+    public float currentTimer = 0f;
+
+    void Update()
+    {
+        if (currentTimer <= 0)
+        {
+            currentTimer = timer;
+            Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, Range);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.gameObject.CompareTag("Monster"))
+                {
+                    collider.GetComponent<Damagable>().ApplyDamage(Damage);
+                }
+            }
+        }
+        currentTimer = currentTimer - Time.deltaTime;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (currentTimer <= 0)
+        {
+            Gizmos.DrawWireSphere(gameObject.transform.position, 2.4f);
+        }
+    }
+}
